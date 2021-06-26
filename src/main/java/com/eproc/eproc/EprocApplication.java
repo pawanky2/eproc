f70c0cca66aaa60934ebf29ed9ed4d3858125baf
pawanky2 <er.pawanky2@gmail.com>
2021-06-26 16:02:55 +0530

    Add files via upload

---
+++ src/main/java/com/eproc/eproc/EprocApplication.java
@@ -0,0 +1,13 @@
+package com.eproc.eproc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EprocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eproc/eproc/EprocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eproc.eproc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
